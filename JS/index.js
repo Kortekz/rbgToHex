b77a0a8bed1eabfdb1bb2b9f66f625e17aefadc0
Kortekz <87400262+Kortekz@users.noreply.github.com>
2023-11-22 23:20:32 +0200

    Push

---
+++ JS/index.js
@@ -0,0 +1,24 @@
+// Select the button and input fields
+let button = document.querySelector('[data-btn]')
+let redInput = document.querySelector('[data-red]')
+let greenInput = document.querySelector('[data-green]')
+let blueInput = document.querySelector('[data-blue]')
+let hexOutput = document.querySelector('[data-hex]')
+
+// Function to convert RGB to HEX
+function rgbToHex(r, g, b) {
+    // Convert each RGB value to hexadecimal and concatenate them
+    let hexCode = '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+    return hexCode
+}
+// Event listener for the button click
+button.addEventListener('click', function() {
+    // Get user-entered RGB values
+    let red = parseInt(redInput.value)
+    let green = parseInt(greenInput.value)
+    let blue = parseInt(blueInput.value)
+    // Convert RGB to HEX
+    const hexCode = rgbToHex(red, green, blue)
+    // Display the HEX code
+    hexOutput.value = hexCode
+})
